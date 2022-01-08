d0f27f2f9e7659122a98a74c612bcb399448fdf4
asceznyk <asceznyk@gmail.com>
2022-01-08 13:56:12 +0530

    ..

---
+++ likesthis.js
@@ -0,0 +1,24 @@
+function likes(names) {
+	let suffix = names.length <= 1 ? 'likes' : 'like'
+	let desc = ''
+	if(!names.length) {
+		return 'no one ' + suffix + ' this'
+	} else {
+		if(names.length == 1) {
+			return names[0] + ' ' + suffix + ' this'
+		} else if (names.length == 2) {
+			return names[0] + ' and ' + names[1] + ' ' + suffix + ' this'
+		} else if (names.length == 3) {
+			return names[0] + ', ' + names[1] + ' and ' + names[2] + ' ' + suffix + ' this'
+		} else if (names.length > 3) {
+			return names[0] + ', ' + names[1] + ' and ' + (names.length-2) + ' others ' + suffix + ' this'
+		}
+	}
+}
+
+const reply = likes(['Peter', 'Alex', 'Max', 'Lucas', 'Toby', 'Gian', 'Sasuke']);
+console.log(reply);
+
+
+
+
