7ff7a0e9cc69654df75aa461d1ce4af935d1a273
asceznyk <asceznyk@gmail.com>
2022-01-11 13:37:27 +0530

    ..

---
+++ diagonaldiff.py
@@ -0,0 +1,18 @@
+
+def diagonalDiff(arr):
+    ltrd, rtld = 0, 0 
+    for y in range(len(arr)):
+        ltrd += arr[y][y]
+        rtld += arr[y][(len(arr)-1) - y]
+
+    return abs(ltrd  - rtld)
+
+
+arr = [
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12]
+]
+
+diff = diagonalDiff(arr)
+print(diff)
