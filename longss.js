0f575b94a3906cdd511b4f075d362ee08fd50202
asceznyk <asceznyk@gmail.com>
2022-01-21 14:14:38 +0530

    ..

--- longss.js
+++ longss.js
@@ -1,10 +1,28 @@
-// "aabbccab"
-//
-//
+// "aabbccabccdd" -> "abc"
+// "pwwkew" -> "wke"
+// "abcbacbb" -> "abc" 
+// "bbbbb" => "b"
+// "ckilb_k_d" => "ckilb"
 
 const lengthOfLongestSubstring = (s) => {
-
+	const bestSubstring = (k) => {
+		let ss = '';
+		while(!ss.includes(s[k]) && k < s.length) {
+			ss += s[k];
+			k++;
+		}
+		return ss;
+	}
+	
+	let bss = '';
+	for(let i = 0; i < s.length; i++){
+		let current = bestSubstring(i);
+		if(current.length > bss.length) bss = current;
+	}
+	return bss.length;
 }
 
-
-
+lengthOfLongestSubstring("pwwkew")
+lengthOfLongestSubstring("aabbccabccdd")
+lengthOfLongestSubstring("dvdf")
+lengthOfLongestSubstring("ckilbkd")
