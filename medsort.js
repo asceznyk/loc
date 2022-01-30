972a974ed59af4712153763ad2e9413886983c1d
asceznyk <asceznyk@gmail.com>
2022-01-30 16:23:27 +0530

    ..

---
+++ medsort.js
@@ -0,0 +1,16 @@
+const findMedianSortedArrays = (nums1, nums2)  => {
+	let nums = [nums1, nums2].flat().sort((a, b) => a-b);
+	let median = 0;
+	let total = nums.length;
+	if(nums.length % 2) {
+		median = nums[(total-1)/2];
+	} else {
+		median = (nums[Math.floor((total-1)/2)] + nums[Math.floor(total/2)]) / 2;
+	}
+
+	return median;
+}
+
+let m = findMedianSortedArrays([2,1], [3,4])
+console.log(m)
+
