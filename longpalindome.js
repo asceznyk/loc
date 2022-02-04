b23c58425c6ab2860049448592679f5e0be4a62a
asceznyk <asceznyk@gmail.com>
2022-02-04 13:18:12 +0530

    ..

--- longpalindome.js
+++ longpalindome.js
@@ -1,19 +1,16 @@
 const longestPalindrome = (s) => {	
-	const expandFromMiddle = (ms, left, right) => {
-		if(ms == null || left > right) return '';
-
-		while(left >= 0 && right < ms.length && ms[right] == ms[left]) {
-			right++; left--;	
+	const expandFromMiddle = (ss, left, right) => {
+		if(ss == null || left > right) return '';
+		while(left >= 0 && right < ss.length && ss[right] == ss[left]) {
+			right++; left--;
 		}
-		left++; right--;
-		left = Math.max(0, left); right = Math.min(right, s.length);
-
-		return s.slice(left, right+1);
+		left++; right--;	
+		return s.slice(Math.max(0, left), Math.min(right, s.length)+1);
 	}
 
 	if(s == null || s.length < 1) return '';
 
-	let smax = '';
+	let smax = '';	
 	for (let i = 0; i < s.length; i++) {
 		let options = [expandFromMiddle(s, i, i), expandFromMiddle(s, i, i+1)];	
 		let ls = options.map((x)=> [x, x.length]).reduce((r, a) => {
--- longpalindome2.js
+++ longpalindome2.js
@@ -1,27 +1,28 @@
-const longestPalindrome = (s) => {
-	let palintab = Array(s.length).fill(Array(s.length).fill(0));
-	let i = 0, j = 0;
-	while(i < s.length && j < s.length) {
-		i++; j++;
-		palintab[i][j] = 1;
-	}
-
+const longestPalindrome = (s) => {	
 	let bss = '';
-	let stride = 2;
-	for(let k = 0; k < Math.floor(s.length/stride); k++) {
-		let ss = s.slice(k, k+stride);
-		console.log(ss);
+	let palintab = [];
+
+	for(let f = 0; f < 2; f++) {
+		for(let y = 0; y < s.length; y++) {
+			palintab[y] = new Array(s.length).fill(0);
+			palintab[y][y] = 1;
+			if(s[y] == s[y+f]) {
+				bss = s.slice(y, y+f+1);
+				palintab[y][y+f] = 1;
+			} 
+		}
+	}
+	
+	for(let d = 2; d < s.length; d++) {	
+		for(let k = 0; k < (s.length-d)+1; k++) {		
+			if(s[k] == s[k+d] && palintab[k+1][k+d-1]) {	
+				palintab[k][k+d] = 1;
+				bss = s.slice(k, k+d+1);
+			}
+		}
 	}
 
-	console.log(bss, c)
-	return bss
+	return bss;
 }
 
-longestPalindrome("cbbd")
-longestPalindrome("abbcbababad")
-longestPalindrome("ggoededx")
-longestPalindrome("aacabdkacaa")
-longestPalindrome("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffgggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg")
-longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth")
-longestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
