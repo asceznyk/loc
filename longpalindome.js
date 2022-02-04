b5320a6013f94c97260508f0fa2d30c08182a60e
asceznyk <asceznyk@gmail.com>
2022-02-04 10:59:00 +0530

    ..

--- longpalindome.js
+++ longpalindome.js
@@ -1,33 +1,30 @@
-const longestpalindrome = (s) => {
-	const expandfrommiddle = (ms, left, right) => {
+const longestPalindrome = (s) => {	
+	const expandFromMiddle = (ms, left, right) => {
 		if(ms == null || left > right) return '';
 
 		while(left >= 0 && right < ms.length && ms[right] == ms[left]) {
-			right++;
-			left--;
+			right++; left--;
+			c++;
 		}
-		left++;
-		right--;
+		left++; right--;
 
-		left = math.max(0, left);
-		right = math.min(right, s.length);
-
-		return s.slice(left, right+1);
+		return s.slice(Math.max(0, left), Math.min(right, s.length)+1);
 	}
 
 	if(s == null || s.length < 1) return '';
-	
+
 	let smax = '';
+	let c = 0;
 	for (let i = 0; i < s.length; i++) {
-		let options = [expandfrommiddle(s, i, i), expandfrommiddle(s, i, i+1)];	
+		let options = [expandFromMiddle(s, i, i), expandFromMiddle(s, i, i+1)];	
 		let ls = options.map((x)=> [x, x.length]).reduce((r, a) => {
 			return a[1] > r[1] ? a : r;
-		})[0]
+		})[0];
 		if(ls.length > smax.length) smax = ls;
+		c++;
 	}
 
 	return smax;
 }
-
 
 
