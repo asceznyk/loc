12dfc4f3cdd2b7d4ceff67701a7dd1428e9c8384
asceznyk <asceznyk@gmail.com>
2023-06-09 15:39:35 +0530

    bad solve... but ok!

--- codeforces/bestie.rs
+++ codeforces/bestie.rs
@@ -1,13 +1,67 @@
-fn solve(a:&Vec<i32>) {
-    println!("{}", &a[0]);
+use std::io;
+use std::str::FromStr;
+
+fn gcd(mut a: i64, mut b: i64) -> i64 {
+    while b != 0 { (a, b) = (b, a % b) };
+    a
 }
 
-fn test() {
-    solve(&vec![1,2,3]);
+fn gcd_arr(arr: &Vec<i64>) -> i64 {
+    let mut res = arr[0];
+    for i in 1..arr.len() { res = gcd(res, arr[i]) };
+    res
+}
+
+fn check_gcd(a: &Vec<i64>) -> bool {
+    gcd_arr(&a) == 1
+}
+
+fn solve(a: &mut Vec<i64>) {
+    if check_gcd(&a) { println!("{}", 0); return };
+
+    let n = a.len();
+    for i in (0..n).rev().step_by(1) {
+        let temp1 = a[i];
+        a[i] = gcd(a[i], (i+1) as i64);
+        if check_gcd(&a) { println!("{}", n-i); return };
+
+        for j in (i+1..n).rev().step_by(1) {
+            let temp2 = a[j];
+            a[j] = gcd(a[j], (j+1) as i64); 
+            if check_gcd(&a) { println!("{}", (n-i) + (n-j)); return; }
+            a[j] = temp2;
+        }
+
+        a[i] = temp1;
+    }
+}
+
+#[allow(dead_code)]
+fn read_line() -> String {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("failed to read line");
+ 
+    buffer
+}
+
+#[allow(dead_code)]
+fn read_num<T: FromStr>() -> Result<T, T::Err>{
+    read_line().trim().parse::<T>()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: FromStr>() -> Result<Vec<T>, T::Err> {
+    read_line().split_whitespace().map(|x| x.parse::<T>()).collect()
 }
 
 fn main() {
-    test();
+    let t = read_num::<i32>().unwrap();
+    for _i in 0..t {
+        let _n = read_num::<i32>().unwrap();
+        let mut a = read_vec::<i64>().unwrap();
+        solve(&mut a);
+    }
 }
-
 
