a4d249dac2a6a27823b5d7cf1cbbc2c33ce3357d
asceznyk <asceznyk@gmail.com>
2023-07-31 15:16:19 +0530

    coin change 2 - leetcode

--- basics/countcoins.py
+++ basics/countcoins.py
@@ -5,7 +5,6 @@ def solve(n:int, a:List[int]):
     k = len(a)
     dp = [[0]*(n+1) for _ in range(k+1)]
 
-    for j in range(n+1): dp[0][j] = 0
     for i in range(k+1): dp[i][0] = 1
     for i in range(1, k+1):
         for j in range(1, n+1):
