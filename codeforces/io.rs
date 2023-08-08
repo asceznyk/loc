c0172ab5f8bd97f85acc3989234a6173b1d3bf59
asceznyk <asceznyk@gmail.com>
2023-08-08 19:05:13 +0530

    soln 1845C

--- codeforces/io.rs
+++ codeforces/io.rs
@@ -22,22 +22,5 @@ fn read_vec<T: FromStr>() -> Result<Vec<T>, T::Err> {
     read_line().split_whitespace().map(|x| x.parse::<T>()).collect()
 }
 
-fn solve(n: i64, k: i64) { 
-    let mut p: i64 = 0;
-    for i in 0..k {
-        p += 1 << i;
-        if p > n { break };
-    }
-    println!("{}", 1 + cmp::min(n, p));
-}
-
-fn main() {
-    let t = read_num::<i32>().unwrap();
-    for _ in 0..t {
-        let a = read_vec::<i64>().unwrap();
-        let (n, k) = (a[0], a[1]);
-        solve(n, k);
-    }
-}
 
 
