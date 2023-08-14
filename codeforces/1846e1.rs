a077e189ad048af145de5ec92531516a60ddfd3a
asceznyk <asceznyk@gmail.com>
2023-08-14 15:21:18 +0530

    added soln for 1846e1; changed io.rs

--- codeforces/1846e1.rs
+++ codeforces/1846e1.rs
@@ -18,17 +18,26 @@ fn read_vec<T: FromStr>() -> Result<Vec<T>, T::Err> {
   read_line().split_whitespace().map(|x| x.parse::<T>()).collect()
 }
 
-fn solve() {}
-
-fn test() {}
-
-fn input() {}
-
-fn main() {
-  test();
-  input();
+fn solve(n:isize) {
+  let x = ((n as f32).sqrt().floor()) as isize;
+  for k in 2isize..x+1 {
+    let (mut m, mut t)= (n-1, 1);
+    while m >= 0 {
+      if m == 0 && t > 2 { println!("YES"); return };
+      m -= k.pow(t);
+      t += 1;
+      if m < 0 { break }; 
+    };
+  }
+  println!("NO");
 }
 
-
+fn main() {
+  let t = read_num::<usize>().unwrap();
+  for _ in 0..t {
+    let n = read_num::<isize>().unwrap();
+    solve(n);
+  }
+}
 
 
