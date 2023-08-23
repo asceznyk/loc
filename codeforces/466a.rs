b518b26cf86e423495c1aef75feb63d0cb16a85c
asceznyk <asceznyk@gmail.com>
2023-08-23 13:25:31 +0530

    solved 466A

---
+++ codeforces/466a.rs
@@ -0,0 +1,40 @@
+use std::io;
+use std::cmp::max;
+use std::str::FromStr;
+
+#[allow(dead_code)]
+fn read_line() -> String {
+  let mut buffer = String::new();
+  io::stdin().read_line(&mut buffer).expect("failed to read line");
+  buffer
+}
+
+#[allow(dead_code)]
+fn read_vec<T: FromStr>() -> Result<Vec<T>, T::Err> {
+  read_line().split_whitespace().map(|x| x.parse::<T>()).collect()
+}
+
+fn solve(n:usize, m:usize, a:usize, b:usize) {
+  let mmod = if n%m == 0 {n/m} else {(n/m)+1};
+  let exps = vec![mmod*b, n*a, (n/m)*b+(n-(n/m)*m)*a];
+  println!("{}", *exps.iter().min().unwrap());
+}
+
+fn test() {
+  solve(6, 2, 1, 2);
+  solve(5, 2, 2, 3);
+  solve(10, 3, 5, 1);
+  solve(1, 1000, 1, 2);
+  solve(10, 3, 1, 2);
+}
+
+fn main() {
+  //test();
+  let x = read_vec::<usize>().unwrap();
+  let (n, m, a, b) = (x[0], x[1], x[2], x[3]);
+  solve(n, m, a, b);
+}
+
+
+
+
