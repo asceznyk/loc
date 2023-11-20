177383d178284a24fc756330705fdbf73006d25c
asceznyk <asceznyk@gmail.com>
2023-11-20 20:15:52 +0530

    solved :)

--- leetcode/twosum.js
+++ leetcode/twosum.js
@@ -1,13 +1,10 @@
 const twoSum = (nums, target) => {
-	for(let i = 0; i < nums.length; i++) {
-		for(let j = 0; j < nums.length; j++) {
-			let sum = nums[i] + nums[j];
-			if(sum == target) {
-				if(i !== j) return [i, j]
-			} 
-		}
-	}
+  for(let i = 0; i < nums.length; i++) {
+    for(let j = 0; j < nums.length; j++) {
+      let sum = nums[i] + nums[j];
+      if(sum == target) {
+        if(i !== j) return [i, j];
+      } 
+    }
+  }
 }
-
-let ts = twoSum([0,4,3,0], 0)
-console.log(ts)
