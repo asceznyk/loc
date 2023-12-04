906b58496e11078b660cef5bc585d223c7a7f274
asceznyk <asceznyk@gmail.com>
2023-12-04 14:56:30 +0530

    slight edit

--- leetcode/mostwater.py
+++ leetcode/mostwater.py
@@ -1,17 +1,15 @@
 from typing import List
 
-
-def maxArea(height:List[int]) -> int:
+class Solution:
+  def maxArea(height:List[int]) -> int:
     lp = 0
     rp = len(height)-1
     mwater = 0
-
     while(lp < rp):
-        lh, rh = height[lp], height[rp]
-        area = abs(rp-lp) * min(lh, rh)
-        if area >= mwater: mwater = area
-        if height[lp] <= height[rp]: lp += 1
-        else: rp -= 1
-
+      lh, rh = height[lp], height[rp]
+      area = abs(rp-lp) * min(lh, rh)
+      if area >= mwater: mwater = area
+      if height[lp] <= height[rp]: lp += 1
+      else: rp -= 1
     return mwater
 
