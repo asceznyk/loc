8719b97550a99e59d1bda7329c5a05d156f84cf1
asceznyk <asceznyk@gmail.com>
2023-12-06 15:20:48 +0530

    added soln 271

--- leetcode/271.py
+++ leetcode/271.py
@@ -1,9 +1,22 @@
 from typing import List
 
 class Solution:
-  def encode(self, strs:List[str]):
-    return "*".join(strs)
-  def decode(self, s:str):
-    return s.split("*")
 
+  def encode(self, strs:List[str]):
+    s = ""
+    for w in strs: s += f"{len(w)}|{w}"
+    return s
+
+  def decode(self, s:str):
+    i, j = 0, 0
+    strs = []
+    while i < len(s):
+      c = s[i]
+      if c == '|':
+        n = int(s[j:i])
+        strs.append(s[i+1:i+n+1])
+        i += n
+        j = i+1
+      i += 1
+    return strs
 
