3d3f520bcf891ec8e089ed1647bdee2ab0a9b3d9
asceznyk <asceznyk@gmail.com>
2023-12-18 17:19:32 +0530

    solved :)

--- leetcode/739.py
+++ leetcode/739.py
@@ -3,16 +3,11 @@ from typing import List
 class Solution:
   def dailyTemperatures(self, temperatures:List[int]) -> List[int]:
     stack = []
-    days = [0 for t in temperatures]
+    days = [0] * len(temperatures)
     for i, t in enumerate(temperatures):
-      while stack and i > 0 and t > p:
+      while stack and t > stack[-1][1]:
         j, k = stack.pop()
         days[j] = i-j
-        if k >= t:
-          stack.append((j,k))
-          days[j] = 0
-          break
       stack.append((i,t))
-      p = t
     return days
 
