1160acb8dfbbc243f56fdd18816dcdb114482215
asceznyk <asceznyk@gmail.com>
2023-12-18 12:51:29 +0530

    re-indented

--- leetcode/49.js
+++ leetcode/49.js
@@ -1,14 +1,14 @@
 const groupAnagrams = function(strs) {
-	let group = [];
-	let sortedD = {};
-	for (let i = 0; i < strs.length; i++) {
-		let s = strs[i];
-		let k = s.split('').sort().join('');
-		if (k in sortedD) { sortedD[k].push(s) }
-		else { sortedD[k] = [s]; }
-	}
-	for (const k in sortedD) { group.push(sortedD[k]); }
-	return group;
+  let group = [];
+  let sortedD = {};
+  for (let i = 0; i < strs.length; i++) {
+    let s = strs[i];
+    let k = s.split('').sort().join('');
+    if (k in sortedD) { sortedD[k].push(s) }
+    else { sortedD[k] = [s]; }
+  }
+  for (const k in sortedD) { group.push(sortedD[k]); }
+  return group;
 }
 
 
