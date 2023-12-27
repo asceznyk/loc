96d28bd3f4c46ea649477cacd4e99cbcfe767017
asceznyk <asceznyk@gmail.com>
2023-12-27 18:10:35 +0530

    121 solved :)

--- leetcode/121.js
+++ leetcode/121.js
@@ -1,12 +1,11 @@
 const maxProfit = function(prices) {
-  let dp = []
-  dp[0] = 0
+  let profit = 0;
   let mp = prices[0]
   for (let i = 1; i < prices.length; i++) {
     mp = Math.min(prices[i-1], mp)
     let diff = prices[i] - mp
-    dp[i] = Math.max(diff, dp[i-1])
+    profit = Math.max(diff, profit)
   }
-  return dp[dp.length-1]
+  return profit
 }
 
