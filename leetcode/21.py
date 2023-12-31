db978726798dd3e3efabd7cd4d1f94f7ec022334
asceznyk <asceznyk@gmail.com>
2023-12-31 22:35:36 +0530

    added soln 21 :)

--- leetcode/21.py
+++ leetcode/21.py
@@ -2,17 +2,17 @@ from typing import Optional, List
 
 class Solution:
     def mergeTwoLists(self, list1:Optional[ListNode], list2:Optional[ListNode]) -> Optional[ListNode]:
-      head = ListNode(0)
+      head = ListNode()
       ans = head
       while list1 and list2:
         p1, p2 = list1.val, list2.val
-        if p1 <= p2:
+        if p1 < p2:
           list1 = list1.next
           head.next = ListNode(p1)
         else:
           list2 = list2.next
           head.next = ListNode(p2)
         head = head.next
-      head.next = list1 if list1 else list2
+      head.next = list1 or list2
       return ans.next
 
