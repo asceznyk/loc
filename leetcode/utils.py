955d3b9e0755bc62eead5916474fced0859a1183
asceznyk <asceznyk@gmail.com>
2024-01-03 18:51:49 +0530

    added utils

---
+++ leetcode/utils.py
@@ -0,0 +1,32 @@
+class ListNode:
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = None
+
+def linkedList(arr:List) -> ListNode:
+  if not arr: return None
+  node = ListNode(arr[0])
+  head = node
+  for i in range(1, len(arr)):
+    node.next = ListNode(arr[i])
+    node = node.next
+  return head
+
+def catLL(head:Optional[ListNode]):
+  s = ""
+  while head:
+    s += f"{head.val} -> "
+    head = head.next
+  print(f"{s}None")
+
+def binary_search(self, nums:List[int], target:int) -> int:
+  lp = 0
+  rp = len(nums)-1
+  while lp <= rp:
+    mp = (rp+lp)//2
+    n = nums[mp]
+    if n == target: return True
+    if n < target: lp = mp+1
+    elif n > target: rp = mp-1
+  return False
+
