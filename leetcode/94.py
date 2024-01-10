aaf66bfe1d1226d2c4898bdf17b3b7332d26736b
asceznyk <asceznyk@gmail.com>
2024-01-10 14:17:09 +0530

    didn't get soln; but learnt it :)

--- leetcode/94.py
+++ leetcode/94.py
@@ -1,13 +1,30 @@
 from typing import List, Union
 
+## Recursive
 class Solution:
   def inorderTraversal(self, root:TreeNode) -> List[int]:
-    def rdfs(node:TreeNode) -> None:
-      if node is None: return
+    def rdfs(node:TreeNode):
+      if not node: return
       rdfs(node.left)
       visited.append(node.val)
       rdfs(node.right)
     visited = []
     rdfs(root)
     return visited
+
+## Iterative
+class Solution:
+  def inorderTraversal(self, root:TreeNode) -> List[int]:
+    stack = []
+    visited = []
+    curr = root
+    while stack or curr:
+      while curr:
+        stack.append(curr)
+        curr = curr.left
+      curr = stack.pop()
+      visited.append(curr.val)
+      curr = curr.right
+    return visited
+
 
