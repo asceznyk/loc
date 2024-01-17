beca1d1e65b68645b2ca33742bff503d587a8051
asceznyk <asceznyk@gmail.com>
2024-01-17 19:51:58 +0530

    memo :)

--- misc/minchange.py
+++ misc/minchange.py
@@ -1,15 +1,21 @@
+import time
+
 from typing import List
 
 
 inf = int(1e+9)
 
 
-def solve_dfs(n:int, a:List[int]) -> int:
-    if n == 0: return 0
-    m = inf
-    for x in a:
-        m = min(solve_dfs(n-x,a)+1 if n-x >= 0 else inf, m)
-    return m
+def solve_dfs(n:int, a:List[int]):
+    def dfs(n:int, a:List[int]) -> int:
+        if n == 0: return 0
+        if memo[n] < n+1: return memo[n]
+        for x in a:
+            if n-x < 0: continue
+            memo[n] = min(dfs(n-x,a)+1, memo[n])
+        return memo[n]
+    memo = [n+1 for _ in range(n+1)]
+    print(dfs(n, a))
 
 
 def solve_dp(n:int, a:List[int]):
@@ -17,15 +23,8 @@ def solve_dp(n:int, a:List[int]):
     dp[0] = 0
     for i in range(1, n+1):
         for x in a:
-            dp[i] = min(dp[i], dp[i-x]+1 if i-x >= 0 else inf)
+            if i-x < 0: continue
+            dp[i] = min(dp[i], dp[i-x]+1)
     print(dp[n])
 
-
-def main():
-    solve_dp(11, [5,5,1])
-    solve_dp(6, [1,3,4])
-
-
-if __name__ == '__main__':
-    main()
 
