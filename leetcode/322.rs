82d72775c59811ca12698103c432f8171dbcb6f4
asceznyk <asceznyk@gmail.com>
2024-01-22 12:16:31 +0530

    updated with impl

--- leetcode/322.rs
+++ leetcode/322.rs
@@ -1,17 +1,17 @@
 use std::cmp::min;
 
-fn coin_change(coins:Vec<i32>, amount:i32) -> i32 {
-  let mut dp: Vec<i32> = vec![amount+1; (amount+1) as usize];
-  dp[0] = 0;
-  for i in 1..=amount {
-    let a = i as usize;
-    for x in &coins {
-      if i-x < 0 { continue };
-      dp[a] = min(dp[a-(*x as usize)]+1, dp[a]);
+impl Solution {
+  fn coin_change(coins:Vec<i32>, amount:i32) -> i32 {
+    let mut dp: Vec<i32> = vec![amount+1; (amount+1) as usize];
+    dp[0] = 0;
+    for i in 1..=amount {
+      let a = i as usize;
+      for x in &coins {
+        if i-x < 0 { continue };
+        dp[a] = min(dp[a-(*x as usize)]+1, dp[a]);
+      }
     }
+    let ans = dp[amount as usize];
+    if ans == amount+1 { -1 } else { ans }
   }
-  let ans = dp[amount as usize];
-  if ans == amount+1 { -1 } else { ans }
 }
-
-
