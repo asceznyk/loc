723dce7af03d08141d7a0aedc1f73f332e4b40aa
asceznyk <asceznyk@gmail.com>
2024-01-25 17:16:22 +0530

    solved :)

--- leetcode/39.cpp
+++ leetcode/39.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
       vector<int> comb{};
-      vector<int> idxs{};
       vector<vector<int>> res{};
-      backtrack(target, candidates, comb, idxs, res); 
+      backtrack(target, candidates, comb, 0, res); 
       return res;
     }
 private:
@@ -12,22 +11,19 @@ private:
         int target,
         vector<int>& candidates,
         vector<int>& comb,
-        vector<int>& idxs,
+        int i,
         vector<vector<int>>& res
     ) {
       int csum = accumulate(comb.begin(), comb.end(), 0);
-      if(csum >= target) {
-        if (csum == target) res.push_back(comb);
+      if (csum == target) {
+        res.push_back(comb);
         return;
       }
-      for(int i = 0; i < candidates.size(); i++) {
-        if(idxs.size() && idxs[idxs.size()-1] > i) continue;
-        comb.push_back(candidates[i]);
-        idxs.push_back(i);
-        backtrack(target, candidates, comb, idxs, res);
-        comb.pop_back();
-        idxs.pop_back();
-      }
+      if (csum > target || i >= candidates.size()) return;
+      comb.push_back(candidates[i]);
+      backtrack(target, candidates, comb, i, res);
+      comb.pop_back();
+      backtrack(target, candidates, comb, i+1, res);
     }
 };
 
