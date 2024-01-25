9365263f5f8096936971871c595ecb4c2144a5c3
asceznyk <asceznyk@gmail.com>
2024-01-25 15:43:30 +0530

    solved :)

---
+++ leetcode/39.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+      vector<int> comb{};
+      vector<int> idxs{};
+      vector<vector<int>> res{};
+      backtrack(target, candidates, comb, idxs, res); 
+      for (auto c: res) {
+        printVector(c);
+      }
+      return res;
+    }
+private:
+    void backtrack(
+        int target,
+        vector<int>& candidates,
+        vector<int>& comb,
+        vector<int>& idxs,
+        vector<vector<int>>& res
+    ) {
+      int csum = accumulate(comb.begin(), comb.end(), 0);
+      if(csum >= target) {
+        if (csum == target) {
+          res.push_back(comb);
+        } 
+        return;
+      }
+      for(int i = 0; i < candidates.size(); i++) {
+        if(idxs.size() && idxs[idxs.size()-1] > i) continue;
+        comb.push_back(candidates[i]);
+        idxs.push_back(i);
+        backtrack(target, candidates, comb, idxs, res);
+        comb.pop_back();
+        idxs.pop_back();
+      }
+    }
+};
+
+
