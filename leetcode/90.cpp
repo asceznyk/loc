7070da7f44e6e632df12efd807e66ef0e2b2e3fe
asceznyk <asceznyk@gmail.com>
2024-01-26 22:12:32 +0530

    soln :|

---
+++ leetcode/90.cpp
@@ -0,0 +1,38 @@
+#include "utils.h"
+
+class Solution {
+public:
+  vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    vector<int> ss{};
+    vector<vector<int>> pos{};
+    sort(nums.begin(), nums.end());
+    search(nums, ss, pos, 0);
+    return pos;
+  }
+private:
+  void search(
+    vector<int>& nums,
+    vector<int>& ss,
+    vector<vector<int>>& pos,
+    int k
+  ) {
+    if (k >= nums.size()) {
+      pos.push_back(ss);
+      return;
+    }
+    ss.push_back(nums[k]);
+    search(nums, ss, pos, k+1);
+    ss.pop_back();
+    while(k+1 < nums.size() && nums[k] == nums[k+1]) k++;
+    search(nums, ss, pos, k+1);
+  }
+};
+
+int main() {
+  Solution solve;
+  vector<int> s1 = {1,2,2};
+  solve.subsetsWithDup(s1);
+  vector<int> s2 = {0};
+  solve.subsetsWithDup(s2);
+  return 0;
+}
