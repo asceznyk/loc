34bb98d66d7e70b06a149e8c94eab8a3df2f6f9c
asceznyk <asceznyk@gmail.com>
2024-02-15 12:15:42 +0530

    solved :)

---
+++ cses/1068.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  ll n; 
+  cin >> n;
+  while(n != 1) {
+    cout << n << " ";
+    if(n%2) n = 3*n+1;
+    else n = n/2;
+  }
+  cout << 1 << endl;
+  return 0;
+}
+
