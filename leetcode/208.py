a1b0ffae87679b5f827a5746f4bb614a7d5485cf
asceznyk <asceznyk@gmail.com>
2024-02-21 16:52:30 +0530

    solved :)

--- leetcode/208.py
+++ leetcode/208.py
@@ -6,16 +6,16 @@ class Trie:
   def insert(self, word:str):
     tree = self.trie
     for char in word:
-      if char not in tree: tree[char] = {}
+      if not tree.get(char): tree[char] = {}
       tree = tree[char]
     tree['*'] = True
 
   def search(self, word:str, full:bool=True) -> bool:
     tree = self.trie
     for char in word:
-      if (char not in tree): return False
+      if not tree.get(char): return False
       tree = tree[char]
-    return '*' in tree if full else True
+    return (tree.get('*') is not None) if full else True
 
   def startsWith(self, prefix:str) -> bool:
     return self.search(prefix, False)
