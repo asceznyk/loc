6c2477ae8fdb722c5060052ed383de20c5e9f69b
asceznyk <asceznyk@gmail.com>
2024-02-28 11:06:20 +0530

    updated :)

--- leetcode/875.cpp
+++ leetcode/875.cpp
@@ -1,18 +1,14 @@
 class Solution {
 private:
-  bool totalHrs(vector<int>& piles, int k, int h) {
+  bool totalHrs(vector<int>& piles, int k, int& h) {
     unsigned int total = 0;
-    for(auto p: piles) {
-      int res = (p%k) == 0 ? p/k : (p/k)+1;
-      total += res;
-    }
+    for(auto p: piles) total += p/k + min(p%k, 1);
     return total <= h;
   }
 public:
   unsigned int minEatingSpeed(vector<int>& piles, int h) {
-    auto z = max_element(piles.begin(), piles.end());
     unsigned int x = 0;
-    for(int b = *z; b >= 1; b /= 2) {
+    for(int b = *max_element(piles.begin(), piles.end()); b >= 1; b /= 2) {
       while(!totalHrs(piles, x+b, h)) x += b;
     }
     return x+1;
