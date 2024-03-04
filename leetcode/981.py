6b2697da6c8c0d0d3411efde8cd6c556c7e4b3b0
asceznyk <asceznyk@gmail.com>
2024-03-04 14:21:46 +0530

    solved :X

---
+++ leetcode/981.py
@@ -0,0 +1,24 @@
+from utils import *
+
+class TimeMap:
+
+  def __init__(self):
+    self.tmap = {}
+
+  def set(self, key:str, value:str, timestamp:int):
+    if not self.tmap.get(key): self.tmap[key] = []
+    self.tmap[key].append((value, timestamp))
+
+  def get(self, key:str, timestamp:int) -> str:
+    res = ""
+    vals = self.tmap.get(key, [])
+    a, b = 0, len(vals)-1
+    while a <= b:
+      k = (a+b)//2
+      if vals[k][1] <= timestamp:
+        res = vals[k][0]
+        a = k+1
+      else: b = k-1
+    return res
+
+
