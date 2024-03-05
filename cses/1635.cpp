6752595fcdefd23ca557c88de9b4a86263476caa
asceznyk <asceznyk@gmail.com>
2024-03-05 21:00:31 +0530

    solved :)

---
+++ cses/1635.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int mod = 1e+9+7;
+  int n, x;
+  cin >> n >> x;
+  vector<int> coins(n, 0);
+  vector<int> count(x+1, 0);
+  for(int j = 0; j < n; j++) cin >> coins[j];
+  count[0] = 1;
+  for(int i = 1; i <= x; i++) {
+    for(int j = 0; j < n && i-coins[j] >= 0; j++) {
+      count[i] += count[i-coins[j]];
+      count[i] %= mod;
+    }
+  }
+  cout << count[x] << endl;
+  return 0;
+}
+
