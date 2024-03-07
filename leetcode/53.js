a8d9880e523178cf78e452a7978027f79044c38f
asceznyk <asceznyk@gmail.com>
2024-03-07 16:46:58 +0530

    updated :)

--- leetcode/53.js
+++ leetcode/53.js
@@ -1,9 +1,9 @@
 const maxSubArray = function(nums) {
-  let [s, m] = [0, -10001]
+  let [s, m] = [0, nums[0]];
   for (let n of nums) {
     s = Math.max(n, n+s)
     m = Math.max(m, s)
   }
-  return m;
+  return m
 }
 
