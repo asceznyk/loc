054991a480edecdc8839f1ebedc04dc0122bb885
asceznyk <asceznyk@gmail.com>
2024-03-13 13:26:06 +0530

    updated :)

--- leetcode/167.py
+++ leetcode/167.py
@@ -2,11 +2,11 @@ from typing import List
 
 class Solution:
   def twoSum(self, numbers:List[int], target:int) -> List[int]:
-    lp, rp = 0, len(numbers)-1
-    while (lp < rp):
-      l, r = numbers[lp], numbers[rp]
-      if l+r == target: return [lp+1, rp+1]
-      elif l+r > target: rp -= 1
-      else: lp += 1
+    l, r = 0, len(numbers)-1
+    while (l < r):
+      l, r = numbers[l], numbers[r]
+      if l+r == target: return [l+1, r+1]
+      elif l+r > target: r -= 1
+      else: l += 1
     return [0]
 
