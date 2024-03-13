4ea5edb180b0b0c4d3ded5adcc257e232bc3efc6
asceznyk <asceznyk@gmail.com>
2024-03-13 13:22:34 +0530

    updated :)

--- leetcode/11.py
+++ leetcode/11.py
@@ -2,14 +2,14 @@ from typing import List
 
 class Solution:
   def maxArea(height:List[int]) -> int:
-    lp = 0
-    rp = len(height)-1
+    l = 0
+    r = len(height)-1
     mwater = 0
-    while(lp < rp):
-      lh, rh = height[lp], height[rp]
-      area = abs(rp-lp) * min(lh, rh)
-      if area >= mwater: mwater = area
-      if height[lp] <= height[rp]: lp += 1
-      else: rp -= 1
+    while(l < r):
+      lh, rh = height[l], height[r]
+      area = abs(r-l) * min(lh, rh)
+      mwater = max(area, mwater)
+      if lh <= rh: l += 1
+      else: r -= 1
     return mwater
 
