0c3b63cae0776a6a44910667c7837cad2b0e2495
asceznyk <asceznyk@gmail.com>
2024-03-21 14:50:54 +0530

    solved :)

--- leetcode/57.cpp
+++ leetcode/57.cpp
@@ -47,3 +47,26 @@ public:
   }
 };
 
+class Solution {
+  public:
+  vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    if (intervals.size() == 0) vector<vector<int>> {newInterval};
+    vector<vector<int>> loi;
+    for (int i = 0; i < intervals.size(); i++) {
+      if(newInterval[1] < intervals[i][0]) {
+        loi.push_back(newInterval);
+        copy(intervals.begin()+i, intervals.end(), back_inserter(loi));
+        return loi;
+      } else if(newInterval[0] > intervals[i][1]) loi.push_back(intervals[i]);
+      else {
+        newInterval = {
+          min(newInterval[0], intervals[i][0]),
+          max(newInterval[1], intervals[i][1])
+        };
+      }
+    };
+    loi.push_back(newInterval);
+    return loi;
+  };
+};
+
