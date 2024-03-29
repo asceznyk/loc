454001c839fbc7fab194bf6a011a72d3b51f4077
asceznyk <asceznyk@gmail.com>
2024-03-29 16:09:12 +0530

    solved :)

---
+++ leetcode/213.rs
@@ -0,0 +1,18 @@
+impl Solution {
+  pub fn rob(nums: Vec<i32>) -> i32 {
+    fn seq(nums:&Vec<i32>, a:usize, b:usize) -> i32 {
+      let (mut p, mut q, mut r) = (0i32,0i32,0i32);
+      for i in a..b {
+        r = max(nums[i]+q,p);
+        q = p;
+        p = r;
+      }
+      r
+    }
+    let n = nums.len();
+    if n == 1 { return nums[0] };
+    let ans = max(seq(&nums, 0, n-1), seq(&nums, 1, n));
+    println!("{}", ans);
+    ans
+  }
+}
