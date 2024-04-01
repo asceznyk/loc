9fa6637418e0dba7896b90f81178fd5fd04061c2
asceznyk <asceznyk@gmail.com>
2024-04-01 17:33:12 +0530

    added soln :(

--- leetcode/152.js
+++ leetcode/152.js
@@ -1,3 +1,4 @@
+//Complex solve
 const maxProduct = function(nums) {
   let [maxP,q,c,f] = [Number.MIN_SAFE_INTEGER,1,0,1];
   for (let i = 0; i < nums.length; i++) {
@@ -17,3 +18,18 @@ const maxProduct = function(nums) {
   }
   return maxP;
 }
+
+//Simpler solve
+const maxProduct = function(nums) {
+  let n = nums.length;
+  let [maxP,prefix,suffix] = [Number.MIN_SAFE_INTEGER,1,1];
+  for (let i = 0; i < n; i++) {
+    if(prefix == 0) prefix = 1;
+    if(suffix == 0) suffix = 1;
+    prefix *= nums[i];
+    suffix *= nums[n-i-1];
+    maxP = Math.max(maxP, prefix, suffix);
+  }
+  return maxP;
+}
+
