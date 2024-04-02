3df30f283c5ad0476b8a167ed32b10ac67417fee
asceznyk <asceznyk@gmail.com>
2024-04-02 16:49:44 +0530

    updated

--- cses/1635.cpp
+++ cses/1635.cpp
@@ -3,8 +3,9 @@
 
 using namespace std;
 
+const int MOD = (int)1e9 + 7;
+
 int main() {
-  int mod = 1e+9+7;
   int n, x;
   cin >> n >> x;
   vector<int> coins(n, 0);
@@ -14,7 +15,7 @@ int main() {
   for(int i = 1; i <= x; i++) {
     for(int j = 0; j < n && i-coins[j] >= 0; j++) {
       count[i] += count[i-coins[j]];
-      count[i] %= mod;
+      count[i] %= MOD;
     }
   }
   cout << count[x] << endl;
