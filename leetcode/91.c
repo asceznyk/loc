2306b9bb73ebc4a61c5e20a2a2fac369dbe42cdd
asceznyk <asceznyk@gmail.com>
2024-04-17 12:58:47 +0530

    supersolve :)

--- leetcode/91.c
+++ leetcode/91.c
@@ -1,32 +1,18 @@
-/*#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>*/
-
-int countPos(char* s, int i, int* counts) {
-  if(*(s+i) == '\0') return 1;
-  if(*(s+i) == '0') return 0;
-  if(counts[i] != 0) return counts[i];
-  counts[i] = countPos(s, i+1, counts);
-  if(
-    *(s+i+1) != '\0' &&
-    ((*(s+i)-'0' == 2 && *(s+i+1)-'0' <= 6) || *(s+i)-'0' == 1)
-  ) counts[i] += countPos(s, i+2, counts);
-  return counts[i];
-}
-
 int numDecodings(char* s) {
   int n = strlen(s);
-  int counts[n];
+  int counts[n+1];
   memset(counts, 0, sizeof(counts));
-  int ans = countPos(s, 0, counts);
-  printf("ans = %d\n", ans);
-  return ans;
+  counts[n] = 1;
+  for(int i = n-1; i > -1; i--) {
+    if(*(s+i) == '0') {
+      counts[i] = 0;
+      continue;
+    }
+    counts[i] = counts[i+1];
+    if(
+      *(s+i+1) != '\0' &&
+      ((*(s+i)-'0' == 2 && *(s+i+1)-'0' <= 6) || *(s+i)-'0' == 1)
+    ) counts[i] += counts[i+2];
+  }
+  return counts[0];
 }
-
-/*int main() {
-  numDecodings("12");
-  numDecodings("226");
-  numDecodings("11106");
-  numDecodings("167");
-  numDecodings("3612");
-}*/
