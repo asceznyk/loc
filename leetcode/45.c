3e6b48dedb5e76205118172d1e6cd0485d0c8e7e
asceznyk <asceznyk@gmail.com>
2024-04-23 15:42:59 +0530

    solved :)

--- leetcode/45.c
+++ leetcode/45.c
@@ -1,30 +1,12 @@
-#include <stdio.h>
-
 int jump(int* nums, int numsSize) {
   if (numsSize == 1 || numsSize == 2) return numsSize-1;
   int dp[numsSize];
   for(int i = 0; i < numsSize; i++) dp[i] = i;
-  int k = 0, jumps = 0;
+  int k = 0;
   for(int i = 1; i < numsSize; i++) {
-    if (k+nums[k] < i) {
-      while(k+nums[k] < i) k++;
-      jumps++;
-    }
+    if (k+nums[k] < i) while(k+nums[k] < i) k++;
+    int kjump = 1+dp[k];
+    dp[i] = kjump < dp[i] ? kjump : dp[i];
   }
-  printf("jumps = %d\n", jumps);
-  return jumps;
-}
-
-int main() {
-  int nums1[5] = {2,3,1,1,4};
-  jump(nums1, 5);
-  int nums2[5] = {2,3,0,1,4};
-  jump(nums2, 5);
-  int nums3[2] = {2,1};
-  jump(nums3, 2);
-  int nums4[1] = {0};
-  jump(nums4, 1);
-  int nums5[7] = {4,1,1,3,1,1,1};
-  jump(nums5, 7);
-  return 0;
+  return dp[numsSize-1];
 }
