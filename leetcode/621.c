146133e189605591697a3e501f27c89c12487977
asceznyk <asceznyk@gmail.com>
2024-04-28 16:09:22 +0530

    small detail missed :(

---
+++ leetcode/621.c
@@ -0,0 +1,14 @@
+int leastInterval(char* tasks, int tasksSize, int n) {
+  int counts[26];
+  for(int k = 0; k < 26; k++) *(counts+k) = 0;
+  int m = 0;
+  for(int i = 0; i < tasksSize; i++) {
+    counts[tasks[i]-'A']++;
+    int c = counts[tasks[i]-'A'];
+    m = c > m ? c : m;
+  };
+  int ans = (n+1)*(m-1);
+  for(int i = 0; i < 26; i++)
+    if(counts[i] == m) ans++;
+  return (tasksSize > ans ? tasksSize : ans);
+}
