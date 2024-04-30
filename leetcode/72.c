41b1515a8ffad6136cf2089e6ca335230aea6308
asceznyk <asceznyk@gmail.com>
2024-04-30 14:17:57 +0530

    kinda solved :|

---
+++ leetcode/72.c
@@ -0,0 +1,28 @@
+int min(int a, int b) {
+  return a < b ? a : b;
+}
+
+int minDistance(char* word1, char* word2) {
+  int m = (int)strlen(word1);
+  int n = (int)strlen(word2);
+  int dp[m+1][n+1];
+  for(int i = 0; i < m+1; i++) {
+    for(int j = 0; j < n+1; j++) dp[i][j] = 501;
+  }
+  for(int i = 1; i < m+1; i++) dp[i][0] = i;
+  for(int j = 1; j < n+1; j++) dp[0][j] = j;
+  dp[0][0] = 0;
+  for(int i = 1; i < m+1; i++) {
+    for(int j = 1; j < n+1; j++) {
+      dp[i][j] = min(
+        1+dp[i][j-1],
+        min(
+          1+dp[i-1][j],
+          (int)(word1[i-1] != word2[j-1])+dp[i-1][j-1]
+        )
+      );
+    }
+  }
+  return dp[m][n];
+}
+
