02d866f967c4c9e0262617828446e8e588a7db5a
asceznyk <asceznyk@gmail.com>
2024-05-22 23:03:17 +0530

    space optimized..

--- leetcode/1143.c
+++ leetcode/1143.c
@@ -5,14 +5,16 @@ int max(int a, int b) {
 int longestCommonSubsequence(char* text1, char* text2) {
   int m = strlen(text1);
   int n = strlen(text2);
-  int dp[m+1][n+1];
-  for(int i = 0; i < m+1; i++) dp[i][0] = 0;
-  for(int j = 0; j < n+1; j++) dp[0][j] = 0; 
+  int curr[n+1];
+  for(int j = 0; j < n+1; j++) curr[j] = 0;
+  int prev[n+1];
+  for(int j = 0; j < n+1; j++) prev[j] = 0;
   for(int i = 1; i < m+1; i++) {
     for(int j = 1; j < n+1; j++) {
-      if(text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-      else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+      if(text1[i-1] == text2[j-1]) curr[j] = 1 + prev[j-1];
+      else curr[j] = max(prev[j], curr[j-1]);
     }
+    for(int j = 0; j < n+1; j++) prev[j] = curr[j]; // prev = curr;
   }
-  return dp[m][n];
+  return curr[n];
 };
