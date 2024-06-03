236611c751cd3e1e6eababc4ee147cf1a135958f
asceznyk <asceznyk@gmail.com>
2024-06-03 21:48:56 +0530

    another solution :|

--- leetcode/42.cpp
+++ leetcode/42.cpp
@@ -1,3 +1,4 @@
+// O(n) space
 class Solution {
 public:
   int trap(vector<int>& height) {
@@ -12,6 +13,30 @@ public:
       r = max(r, height[n-i-1]);
     }
     for(int i = 0; i < n; i++) w += max(0, min(lmaxs[i],rmaxs[i])-height[i]);
+    return w; //comment
+  }
+};
+
+// O(1) space
+class Solution {
+public:
+  int trap(vector<int>& height) {
+    int n = height.size();
+    if(n <= 2) return 0;
+    int l = 0, r = n-1, w = 0;
+    int hlmax = height[l], hrmax = height[r];
+    while (l < r) {
+      int hl = height[l], hr = height[r];
+      hlmax = max(hlmax, hl);
+      hrmax = max(hrmax, hr);
+      if (height[l] <= height[r]) {
+        l++;
+        w += max(0, hlmax-hl);
+      } else {
+        r--;
+        w += max(0, hrmax-hr);
+      };
+    }
     return w;
   }
 };
