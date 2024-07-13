20735fad4abd03d46e452ba5a6fa339eeff4c938
asceznyk <asceznyk@gmail.com>
2024-07-13 14:23:48 +0530

    solved :)

---
+++ leetcode/606.py
@@ -0,0 +1,16 @@
+class Solution:
+  def tree2str(self, root:Optional[TreeNode]) -> str:
+    def dfs(node:Optional[TreeNode]):
+      if not node: return
+      slist.append(str(node.val))
+      if node.left is None and node.right is None: return
+      for x in [node.left, node.right]:
+        if x == node.right and x is None: continue
+        slist.append("(")
+        dfs(x)
+        slist.append(")")
+    slist = []
+    dfs(root)
+    ans = "".join(slist)
+    return ans
+
