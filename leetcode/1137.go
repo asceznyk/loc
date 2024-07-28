6457d8f3afdbd83a0524e40a624aa1d191f593e1
asceznyk <asceznyk@gmail.com>
2024-07-28 12:25:56 +0530

    solved :)

---
+++ leetcode/1137.go
@@ -0,0 +1,12 @@
+func tribonacci(n int) int {
+  if(n < 3) {
+    if(n == 0) { return 0 } else { return 1 } 
+  }
+  dp := make([]int, n+1)
+  dp[0] = 0; dp[1] = 1; dp[2] = 1
+  for i := 3; i <= n; i++ {
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+  }
+  return dp[n]
+}
+
