743225adcb03f8d5d54ba6f7cf6a145b40c40a7b
asceznyk <asceznyk@gmail.com>
2024-09-05 22:51:45 +0530

    easy :)

---
+++ leetcode/1768.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func mergeAlternately(word1 string, word2 string) string {
+  n, m := len(word1), len(word2)
+  p1, p2, x := 0, 0, 0
+  ans := ""
+  for x < min(n, m) {
+    ans += string(word1[p1]) + string(word2[p2])
+    p1++
+    p2++
+    x++ 
+  }
+  if x >= n {
+    ans += string(word2[x:m])
+  } else {
+    ans += string(word1[x:n])
+  }
+  return ans
+}
+
+func main() {
+  mergeAlternately("abcd", "efghij")
+  mergeAlternately("efghij", "abcd")
+}
