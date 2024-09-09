5e351484815cb11b10e2b28335e6e05287707ccf
asceznyk <asceznyk@gmail.com>
2024-09-09 15:32:55 +0530

    solved :)

---
+++ leetcode/658.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func findClosestElements(arr []int, k int, x int) []int {
+
+}
+
+func main() {
+  findClosestElements([]int{1,2,3,4,5}, 4, 3)
+  findClosestElements([]int{1,2,3,4,5}, 4, -1)
+}
+
