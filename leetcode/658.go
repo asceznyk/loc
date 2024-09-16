b0c6804ef9e22510c736cb46d71e8840ef0ae9a9
asceznyk <asceznyk@gmail.com>
2024-09-16 13:50:54 +0530

    solved :X

--- leetcode/658.go
+++ leetcode/658.go
@@ -1,13 +1,33 @@
-package main
-
-import "fmt"
+func absInt(x int) int {
+  if x < 0 {
+    return -x
+  }
+  return x
+} 
 
 func findClosestElements(arr []int, k int, x int) []int {
-
-}
-
-func main() {
-  findClosestElements([]int{1,2,3,4,5}, 4, 3)
-  findClosestElements([]int{1,2,3,4,5}, 4, -1)
+  n := len(arr)
+  if x <= arr[0] {
+    return arr[:k]
+  }
+  if k >= n {
+    return arr
+  }
+  prefix := make([]int, n)
+  p := 0 
+  for i := 0; i < n; i++ {
+    p += absInt(x - arr[i])
+    prefix[i] = p
+  }
+  j, q, m := 0, 0, 100000000
+  for i := 0; i <= n-k; i++ {
+    s := prefix[i+k-1] - q
+    if s < m {
+      m = s
+      j = i
+    }
+    q = prefix[i]
+  }
+  return arr[j:j+k]
 }
 
