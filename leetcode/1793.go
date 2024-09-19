8aadc29d1a1a0fed5243e8ac2890748ede42ee1d
asceznyk <asceznyk@gmail.com>
2024-09-19 15:16:13 +0530

    solved :X

---
+++ leetcode/1793.go
@@ -0,0 +1,28 @@
+func maximumScore(nums []int, k int) int {
+  n, l, r, m := len(nums), k, k, nums[k]
+  score := 0
+  for l > -1 && r < n {
+    if nums[l] >= nums[r] {
+      m = min(m, nums[l])
+      score = max(score, m*(r-l))
+      l--
+    } else {
+      m = min(m, nums[r])
+      score = max(score, m*(r-l))
+      r++
+    }
+  }
+  for l > -1 {
+    m = min(m, nums[l])
+    score = max(score, m*(r-l))
+    l--
+  }
+  for r < n {
+    m = min(m, nums[r])
+    score = max(score, m*(r-l))
+    r++
+  }
+  return score
+}
+
+
