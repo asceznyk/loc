221266e256aa8c72429610526099f1d36f1545d0
asceznyk <asceznyk@gmail.com>
2024-09-27 22:33:11 +0530

    wip

--- leetcode/665.py
+++ leetcode/665.py
@@ -10,7 +10,7 @@ class Solution:
         return False
       if p > x:
         nums[i-1] = x
-        if (nums[i-2] if i-2 > 0 else 0) > nums[i-1]:
+        if (nums[i-2] if i-2 >= 0 else 0) > nums[i-1]:
           nums[i-1] = p
           nums[i] = p
         once = True
