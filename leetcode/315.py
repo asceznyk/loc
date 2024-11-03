5bdbd1aca776fa14030f3b9295c353658ae3365f
asceznyk <asceznyk@gmail.com>
2024-11-03 15:07:07 +0530

    solved :|

---
+++ leetcode/315.py
@@ -0,0 +1,13 @@
+from sortedcontainers import SortedList
+
+class Solution:
+  def countSmaller(self, nums:List[int]) -> List[int]:
+    oset = SortedList([])
+    res = []
+    for x in reversed(nums):
+      i = oset.bisect_left(x)
+      res.append(i)
+      oset.add(x)
+    return list(reversed(res))
+
+    
