b10e3feda52307351b93934b51576c8e7b634c66
asceznyk <asceznyk@gmail.com>
2024-11-28 17:30:48 +0530

    alternative..

--- leetcode/1498.py
+++ leetcode/1498.py
@@ -1,3 +1,5 @@
+MOD = (10**9)+7
+
 class Solution:
   def numSubseq(self, nums:List[int], target:int) -> int:
     def count(nums:List[int], target:int, l:int, r:int, sign:int) -> int:
@@ -20,5 +22,23 @@ class Solution:
         return sign*int(2*nums[l] <= target) if sign == 1 else sign*int(2*nums[l] > target)
       return sign*((2**(r-l+1))-1) + count(nums, target, l, r, -1*sign)
     nums.sort()
-    return count(nums, target, 0, len(nums)-1, 1) % ((10**9)+7)
+    return count(nums, target, 0, len(nums)-1, 1) % MOD
+
+class Solution:
+  def numSubseq(self, nums:List[int], target:int) -> int:
+    nums.sort()
+    n = len(nums)
+    powers = []
+    k = 1
+    for i in range(n):
+      powers.append(k)
+      k = (k*2) % MOD
+    count, j = 0, n-1
+    for i in range(n):
+      if 2*nums[i] > target: break
+      while j and nums[j] + nums[i] > target:
+        j -= 1
+      count += powers[j-i]
+    return count % MOD
+
 
