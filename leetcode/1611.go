0a7bfe7518dae5d6e8eacbc5d7bfe0cf16b783d0
asceznyk <asceznyk@gmail.com>
2024-12-16 14:14:17 +0530

    implemented :|

--- leetcode/1611.go
+++ leetcode/1611.go
@@ -1,6 +1,3 @@
-func powInt(x int, y int) int {
-  return int(math.Pow(float64(x), float64(y)))
-}
 
 func minimumOneBitOperations(n int) int {
   if n == 0 {
@@ -12,6 +9,6 @@ func minimumOneBitOperations(n int) int {
     k++
   }
   x >>= 1
-  return powInt(2, k) - 1 - minimumOneBitOperations(n^x)
+  return (1 << k) - 1 - minimumOneBitOperations(n^x)
 }
 
