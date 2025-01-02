2d7f4cd2949b3d10c8374400b906ed63efcc45b5
asceznyk <asceznyk@gmail.com>
2025-01-02 16:07:19 +0530

    solved :)

---
+++ leetcode/645.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  vector<int> findErrorNums(vector<int>& nums) {
+    printVector(nums);
+    int n = nums.size(), rep = 0;
+    vector<int> hmap(n+1);
+    for (int i = 0; i < n; i++) {
+      int curr = nums[i];
+      if (hmap[curr] > 0) {
+        rep = curr;
+      }
+      hmap[curr]++;
+    }
+    int i = 0;
+    for (i = 1; i < n+1 && hmap[i] != 0; i++);
+    return {rep, i};
+  }
+};
