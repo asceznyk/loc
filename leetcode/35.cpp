59844d75c4c90f002512a441e900b24cd7496707
asceznyk <asceznyk@gmail.com>
2025-01-09 13:33:25 +0530

    solved :)

---
+++ leetcode/35.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  int searchInsert(vector<int>& nums, int target) {
+    int n = nums.size();
+    int l = 0, r = n-1;
+    while (l <= r) {
+      int m = (l+r)/2;
+      if (nums[m] > target) r = m-1;
+      else if (nums[m] < target) l = m+1;
+      else return m;
+    }
+    return max(l,r);
+  }
+};
+
