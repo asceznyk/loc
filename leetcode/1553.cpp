ca2b6864afb1cc7aad7b2cdcf74c507b4d3f07bd
asceznyk <asceznyk@gmail.com>
2025-01-15 20:01:04 +0530

    solved :)

--- leetcode/1553.cpp
+++ leetcode/1553.cpp
@@ -3,8 +3,7 @@ public:
   int dfs(int n, map<int,int>& dp) {
     if (n <= 1) return n;
     if (dp[n] != 0) return dp[n];
-    dp[n] = 1+min((n%3)+dfs(n/3,dp), (n%2)+dfs(n/2,dp));
-    return dp[n];
+    return dp[n] = 1+min((n%3)+dfs(n/3,dp), (n%2)+dfs(n/2,dp));
   }
   int minDays(int n) {
     map<int,int> dp;
