823f05e32f366b436f55eb432eebd6fe174d8699
asceznyk <asceznyk@gmail.com>
2025-01-15 16:44:12 +0530

    implemented :|

--- leetcode/552.cpp
+++ leetcode/552.cpp
@@ -4,22 +4,24 @@ const int MOD = 1e9+7;
 class Solution {
 public:
   int checkRecord(int n) {
-    vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(3, vector<int>(4)));
+    vector<vector<int>> curr(3, vector<int>(4));
+    vector<vector<int>> prev(3, vector<int>(4));
     for (int cntA = 0; cntA < 2; cntA++) {
-      for (int cntL = 0; cntL < 3; cntL++) dp[0][cntA][cntL] = 1;
+      for (int cntL = 0; cntL < 3; cntL++) prev[cntA][cntL] = 1;
     }
-    for (int i = 1; i < n+1; i++) {
+    for (int i = 0; i < n; i++) {
       for (int cntA = 1; cntA > -1; cntA--) {
         for (int cntL = 2; cntL > -1; cntL--) {
           ll total = 0;
-          total += dp[i-1][cntA][0] % MOD;
-          total += dp[i-1][cntA+1][0] % MOD;
-          total += dp[i-1][cntA][cntL+1] % MOD;
-          dp[i][cntA][cntL] = (int)(total % MOD);
+          total += prev[cntA][0] % MOD;
+          total += prev[cntA+1][0] % MOD;
+          total += prev[cntA][cntL+1] % MOD;
+          curr[cntA][cntL] = (int)(total % MOD);
         }
       }
+      prev = curr;
     }
-    return dp[n][0][0];
+    return curr[0][0];
   }
 };
 
