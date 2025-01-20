37e14315dc0dede83ccd95c32c77a40e0a87634b
asceznyk <asceznyk@gmail.com>
2025-01-20 20:09:17 +0530

    solved :)

---
+++ leetcode/148.cpp
@@ -0,0 +1,24 @@
+#include "../utils.h"
+
+class Solution {
+public:
+  ListNode* sortList(ListNode* head) {
+    vector<int> snums;
+    ListNode* curr = head;
+    while (curr != nullptr) {
+      snums.push_back(curr->val);
+      curr = curr->next;
+    }
+    int n = snums.size();
+    if (n == 0) return nullptr;
+    sort(snums.begin(), snums.end());
+    ListNode* node = new ListNode(snums[0]);
+    ListNode* origin = node;
+    for (int i = 1; i < n; i++) {
+      node->next = new ListNode(snums[i]);
+      node = node->next; 
+    }
+    return origin;
+  }
+};
+
