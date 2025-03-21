76224bde9cacb8a9bbb3ebaf132579dddf380174
asceznyk <asceznyk@gmail.com>
2025-03-21 11:35:48 +0530

    solved :|

--- leetcode/2872.cpp
+++ leetcode/2872.cpp
@@ -4,22 +4,21 @@ class Solution {
 public:
   int ans = 0;
   ll dfs(
-    int i, int k, vector<int>& values,
-    map<int,vector<int>>& adjList, vector<int>& visited
+    int u, int p, int k, vector<int>& values,
+    map<int,vector<int>>& adjList
   ) {
-    ll curr = values[i];
-    if ((int)adjList[i].size() == 0) {
+    ll curr = values[u];
+    if ((int)adjList[u].size() == 0) {
       if (curr % k != 0) {
         ans--;
         return curr;
       }
       return 0LL;
     }
-    visited[i] = 1;
     ll nsum = curr;
-    for(auto& j: adjList[i]) {
-      if (visited[j]) continue;
-      ll inter = dfs(j, k, values, adjList, visited);
+    for(auto& q: adjList[u]) {
+      if (q == p) continue;
+      ll inter = dfs(q, u, k, values, adjList);
       nsum += inter;
     }
     if (nsum % k != 0) {
@@ -37,8 +36,7 @@ public:
       adjList[edge[1]].push_back(edge[0]);
     }
     ans = n;
-    vector<int> visited(n);
-    dfs(0, k, values, adjList, visited);
+    dfs(0, -1, k, values, adjList);
     return ans;
   }
 };
