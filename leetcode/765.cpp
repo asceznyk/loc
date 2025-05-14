62ec534b94445e604a881ecabb844c2a85e0688c
asceznyk <asceznyk@gmail.com>
2025-05-14 19:58:00 +0530

    solved :)

--- leetcode/765.cpp
+++ leetcode/765.cpp
@@ -30,12 +30,8 @@ public:
   int minSwapsCouples(vector<int>& row) {
     int n = row.size()/2;
     DSU dsu(2*n);
-    for (int i = 0; i < n; i++)
-      dsu.unionSet(row[2*i], row[(2*i)+1]); 
-    for (int i = 0; i < n; i++) {
-      printf("a = %d, b = %d\n", 2*i, 2*i+1);
-      dsu.unionSet(2*i, (2*i)+1);
-    }
+    for (int i = 0; i < n; i++) dsu.unionSet(row[2*i], row[(2*i)+1]); 
+    for (int i = 0; i < n; i++) dsu.unionSet(2*i, (2*i)+1);
     set<int> comps;
     for (int i = 0; i < 2*n; i++) comps.insert(dsu.find(i));
     int c = comps.size();
