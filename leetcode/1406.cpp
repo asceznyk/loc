48daf840748dbd25f9bf87a7eccf633c1c5631f5
asceznyk <asceznyk@gmail.com>
2025-05-16 11:43:48 +0530

    updated!

--- leetcode/1406.cpp
+++ leetcode/1406.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-  int dfs(int i, int p, vector<int>& stoneValue, vector<vector<int>>& mem) {
+  int dfs(int i, vector<int>& stoneValue, vector<int>& mem) {
     int n = stoneValue.size();
     if (i > n-1) return 0;
     if (i == n-1) return stoneValue[n-1];
-    if (mem[i][p] != INT_MIN) return mem[i][p];
+    if (mem[i] != INT_MIN) return mem[i];
     int score = INT_MIN;
     int prefix = 0;
     for (int j = i; j < i+3; j++) {
       if (j > n-1) continue;
       prefix += stoneValue[j];
-      score = max(score, prefix-dfs(j+1, p^1, stoneValue, mem));
+      score = max(score, prefix-dfs(j+1, stoneValue, mem));
     }
-    mem[i][p] = score;
-    return mem[i][p];
+    mem[i] = score;
+    return mem[i];
   }
   string stoneGameIII(vector<int>& stoneValue) {
     int n = stoneValue.size();
-    vector<vector<int>> mem(n, vector<int>(2, INT_MIN));
-    int res = dfs(0, 0, stoneValue, mem);
+    vector<int> mem(n, INT_MIN);
+    int res = dfs(0, stoneValue, mem);
     if (res > 0) return "Alice";
     else if(res < 0) return "Bob";
     else return "Tie";
