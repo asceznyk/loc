06635bf8db0a54ad13845c04bcd3dafac07aee98
asceznyk <asceznyk@gmail.com>
2025-05-17 12:50:00 +0530

    updated!

--- leetcode/1140.cpp
+++ leetcode/1140.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-  int dfs(int i, int m, int p, vector<int>& piles, vector<vector<vector<int>>>& mem) {
+  int dfs(int i, int m, vector<int>& piles, vector<vector<int>>& mem) {
     int n = piles.size();
     if (i > n-1) return 0;
-    if (i == n-1) return (!p)*piles[n-1];
-    if (mem[i][m][p] != INT_MIN) return mem[i][m][p];
-    int score = INT_MIN, x = 0, mpref = 0, prefix = 0;
+    if (mem[i][m] != INT_MIN) return mem[i][m];
+    int score = INT_MIN, mpref = 0, prefix = 0;
     for (int j = i; j < i+(2*m); j++) {
       if (j > n-1) continue;
-      x++;
       prefix += piles[j];
-      int calc = dfs(j+1, max(m,x), p^1, piles, mem);
-      score = max(score, (prefix*(!p))-calc);
-    }
-    mem[i][m][p] = score;
-    return mem[i][m][p];
+      score = max(score, prefix-dfs(j+1, max(m,(j-i)+1), piles, mem));
+    } 
+    return mem[i][m] = score;
   }
   int stoneGameII(vector<int>& piles) {
     int n = piles.size();
-    vector<vector<vector<int>>> mem(n, vector<vector<int>>(n, vector<int>(2, INT_MIN)));
-    return  dfs(0, 1, 0, piles, mem);
+    vector<vector<int>> mem(n, vector<int>(n+1, INT_MIN));
+    int total = accumulate(piles.begin(), piles.end(), 0);
+    return (total+dfs(0, 1, piles, mem))/2;
   }
 };
 
