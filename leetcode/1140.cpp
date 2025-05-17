46ca1911f458877cba4f0ddbbb09af00a291198a
asceznyk <asceznyk@gmail.com>
2025-05-17 12:13:18 +0530

    updated!

--- leetcode/1140.cpp
+++ leetcode/1140.cpp
@@ -3,16 +3,15 @@ public:
   int dfs(int i, int m, int p, vector<int>& piles, vector<vector<vector<int>>>& mem) {
     int n = piles.size();
     if (i > n-1) return 0;
-    if (i == n-1) return (p == 0) ? piles[n-1] : 0;
+    if (i == n-1) return (!p)*piles[n-1];
     if (mem[i][m][p] != INT_MIN) return mem[i][m][p];
-    int score = (p == 0) ? INT_MIN : INT_MAX, x = 0, mpref = 0, prefix = 0;
+    int score = INT_MIN, x = 0, mpref = 0, prefix = 0;
     for (int j = i; j < i+(2*m); j++) {
       if (j > n-1) continue;
       x++;
       prefix += piles[j];
       int calc = dfs(j+1, max(m,x), p^1, piles, mem);
-      if (p == 0) score = max(score, prefix+calc);
-      else score = min(score, calc);
+      score = max(score, (prefix*(!p))-calc);
     }
     mem[i][m][p] = score;
     return mem[i][m][p];
