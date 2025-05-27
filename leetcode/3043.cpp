3ad4092eb585ef97815a5211e6de9f1241088604
asceznyk <asceznyk@gmail.com>
2025-05-27 11:41:43 +0530

    solved :)

---
+++ leetcode/3043.cpp
@@ -0,0 +1,59 @@
+class TrieNode {
+public:
+  TrieNode* child[10];
+  bool isNum;
+  TrieNode() {
+    isNum = false;
+    for (auto &a : child) a = nullptr;
+  }
+};
+
+class Trie {
+public:
+  TrieNode* root;
+  Trie() {
+    root = new TrieNode();
+  }
+  void insert(string s) {
+    TrieNode* p = root;
+    for (auto &a: s) {
+      int i = a-'0';
+      if (!p->child[i]) p->child[i] = new TrieNode();
+      p = p->child[i];
+    }
+    p->isNum = true;
+  }
+  int commonPref(string s) {
+    TrieNode* p = root;
+    int cnt = 0;
+    for (auto &a: s) {
+      int i = a-'0';
+      if (!p->child[i]) break;
+      p = p->child[i];
+      cnt++;
+    }
+    return cnt;
+  }
+};
+
+class Solution {
+public:
+  int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+    printf("arr1 = "); printVector(arr1);
+    printf("arr2 = "); printVector(arr2);
+    int ans = 0;
+    int n = arr1.size(), m = arr2.size();
+    Trie* trie = new Trie();
+    for (int i = 0; i < n; i++) {
+      string snum = to_string(arr1[i]);
+      trie->insert(snum);
+    }
+    for (int j = 0; j < m; j++) {
+      string curr = to_string(arr2[j]);
+      ans = max(ans, trie->commonPref(curr));
+    }
+    printf("ans = %d\n", ans);
+    return ans;    
+  }
+};
+
