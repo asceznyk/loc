1a36109aa999a89b358945dafc9a777fcfa50d23
asceznyk <asceznyk@gmail.com>
2025-08-01 12:09:55 +0530

    updated!

--- leetcode/689.cpp
+++ leetcode/689.cpp
@@ -5,13 +5,12 @@ public:
     vector<int>& prefix, vector<vector<int>>& mem
   ) {
     int n = prefix.size();
-    if (t == 0) return 0;
-    if (i >= n-k) return (t > 0 ? INT_MIN : 0);
+    if (i >= n-k || t == 0) return 0;
     if (mem[i][t] != -1) return mem[i][t];
-    int skip = dfs(i+1, t, k, prefix, mem);
-    int take = (prefix[i+k] - prefix[i]) + \
-               dfs(i+k, t-1, k, prefix, mem);
-    return mem[i][t] = max(skip, take);
+    return mem[i][t] = max(
+      dfs(i+1, t, k, prefix, mem),
+      (prefix[i+k] - prefix[i]) + dfs(i+k, t-1, k, prefix, mem)
+    );
   }
   void reconst(
     int i, int t, int k,
@@ -38,7 +37,6 @@ public:
     }
     vector<int> indices;
     vector<vector<int>> mem(n+1, vector<int>(4, -1));
-    int maxSum = dfs(0, 3, k, prefix, mem);
     reconst(0, 3, k, indices, prefix, mem);
     return indices;
   }
