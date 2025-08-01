73e557911fd3f237a8345511582e8e00985bd626
asceznyk <asceznyk@gmail.com>
2025-08-01 15:17:47 +0530

    solved :)

---
+++ leetcode/2050.py
@@ -0,0 +1,22 @@
+class Solution:
+  def minimumTime(self, n:int, relations:List[List[int]], time:List[int]) -> int:
+    def dfs(i:int) -> int:
+      if mem[i] != -1: return mem[i]
+      pSum = time[i] 
+      for j in adj[i]:
+        pSum = max(time[i]+dfs(j), pSum)
+      mem[i] = pSum
+      return mem[i]
+    indegree = [0 for _ in range(n)]
+    adj = [[] for _ in range(n)]
+    for edge in relations:
+      [a,b] = edge
+      indegree[b-1] += 1
+      adj[a-1].append(b-1)
+    nodes = []
+    roots = [i for i, v in enumerate(indegree) if v == 0]
+    mem = [-1 for _ in range(n+1)]
+    ans = 0
+    for root in roots: ans = max(ans, dfs(root))
+    return ans
+
