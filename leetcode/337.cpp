0603e55f41757666e356a6001c2051e30cdd0181
asceznyk <asceznyk@gmail.com>
2025-08-03 14:28:33 +0530

    solved :)

---
+++ leetcode/337.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+  struct PointerHash {
+    size_t operator()(const TreeNode* ptr) const {
+      return hash<uintptr_t>{}(reinterpret_cast<uintptr_t>(ptr));
+    }
+  };
+  int dfs(
+    TreeNode* node,
+    unordered_map<TreeNode*,int, PointerHash>& dp
+  ) {
+    if (node == nullptr) return 0;
+    if (dp.count(node) > 0) return dp[node];
+    int compVal = 0;
+    if (node->left != nullptr)
+      compVal += dfs(node->left->left, dp) + \
+                 dfs(node->left->right, dp);
+    if (node->right != nullptr)
+      compVal += dfs(node->right->left, dp) + \
+                 dfs(node->right->right, dp);
+    return dp[node] = max(
+      node->val + compVal, 
+      dfs(node->left, dp) + dfs(node->right, dp)
+    );
+  }
+  int rob(TreeNode* root) {
+    unordered_map<TreeNode*, int, PointerHash> dp; 
+    int ans = dfs(root, dp);
+    printf("ans = %d\n", ans);
+    return ans;
+  }
+};
+
