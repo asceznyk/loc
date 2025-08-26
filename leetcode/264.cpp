c8539e978202c365af9d024851e2dd6889211350
asceznyk <asceznyk@gmail.com>
2025-08-26 14:08:41 +0530

    solved :)

--- leetcode/264.cpp
+++ leetcode/264.cpp
@@ -7,16 +7,16 @@ public:
     priority_queue<ll> pq;
     pq.push(-1LL);
     vector<int> primes = {2,3,5};
+    ll ans = 1;
     while (!pq.empty()) {
       if (n == 0) break;
-      int x = -1LL*pq.top(); pq.pop();
+      ll x = -1LL*pq.top(); pq.pop();
+      ans = x;
       if (mem.find(x) != mem.end()) continue;
       mem[x] = 1;
       n--;
       for (int y: primes) pq.push(-1LL*x*y);
     }
-    ll ans = 1;
-    for (auto& p: mem) ans = max(ans, p.first);
     return static_cast<int>(ans);
   }
 };
