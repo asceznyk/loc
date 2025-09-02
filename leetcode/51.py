d185e84f84438d2831f608afb9262173ddba823e
asceznyk <asceznyk@gmail.com>
2025-09-02 19:43:16 +0530

    solved :)

---
+++ leetcode/51.py
@@ -0,0 +1,21 @@
+class Solution:
+  def solveNQueens(self, n:int) -> List[List[str]]:
+    def backtrack(i:int):
+      if i == n:
+        res.append(["".join([c for c in row]) for row in board])
+        return
+      for j in range(n):
+        if columns[j] or diag1[i+j] or diag2[j-i+n-1]: continue
+        board[i][j] = "Q"
+        columns[j] = diag1[i+j] = diag2[j-i+n-1] = 1
+        backtrack(i+1)
+        columns[j] = diag1[i+j] = diag2[j-i+n-1] = 0
+        board[i][j] = "."
+    res = []
+    board = [["." for _ in range(n)] for _ in range(n)]
+    columns = [0 for _ in range(n)]
+    diag1 = [0 for _ in range(2*n-1)]
+    diag2 = [0 for _ in range(2*n-1)]
+    backtrack(0)
+    return res
+
